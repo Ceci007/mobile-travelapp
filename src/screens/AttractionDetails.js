46de9b93c2f3caa048bbcb341f549f80291c4801
Ceci <followmyfriends32@gmail.com>
2023-01-22 11:21:11 -0300

    images from params not showing

--- src/screens/AttractionDetails.js
+++ src/screens/AttractionDetails.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableOpacity, Dimensions} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  Dimensions,
+  Image,
+  View,
+  Pressable,
+} from 'react-native';
 import styled from 'styled-components/native';
 
 const {height} = Dimensions.get('window');
 
 const AttractionDetails = ({route, navigation}) => {
   const {item} = route?.params || {};
-  const mainImage = item?.images.length ? item?.images[0] : null;
+  const mainImage = item?.images?.length ? item?.images[0] : null;
 
+  console.log(item?.images);
   const onBack = () => {
     navigation.goBack();
+  };
+
+  const onGalleryNavigate = () => {
+    navigation.navigate('Gallery', {images: item?.images});
   };
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <Wrapper>
-        <ImageBackground source={{uri: mainImage}} />
+        <ImageBackground source={{uri: mainImage}}>
+          <ImageBox>
+            <Pressable hitSlop={8} onPress={onBack}>
+              <Image
+                style={{width: 36, height: 36}}
+                source={require('../assets/back.png')}
+              />
+            </Pressable>
+            <Pressable hitSlop={8}>
+              <Image
+                style={{width: 36, height: 36}}
+                source={require('../assets/share.png')}
+              />
+            </Pressable>
+          </ImageBox>
+        </ImageBackground>
       </Wrapper>
-      <Text>{item.name}</Text>
-      <TouchableOpacity onPress={onBack}>
-        <Text>Go back</Text>
-      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {item?.imgaes?.length
+          ? item?.images?.map(image => (
+              <Image
+                style={{width: 40, height: 40, backgroundColor: '#000'}}
+                key={image}
+                source={{uri: image}}
+              />
+            ))
+          : null}
+      </View>
+      <Text>{item?.name}</Text>
     </SafeAreaView>
   );
 };
@@ -30,10 +70,19 @@ export default React.memo(AttractionDetails);
 const ImageBackground = styled.ImageBackground`
   width: 100%;
   height: ${height / 2}px;
+  position: relative;
+  z-index: -1;
 `;
 
 const Wrapper = styled.View`
   margin: 32px;
   border-radius: 20px;
   overflow: hidden;
+`;
+
+const ImageBox = styled.View`
+  padding: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
